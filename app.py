9eba62e0d580743903e26456a1f47ed78635b282
Rajayne <ryann.j.nealey@gmail.com>
2023-03-28 14:26:16 -0700

    Create Pet model

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template,  redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from forms import 
+from models import db,  connect_db, Pet
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.app_context().push()
+
+app.config['SECRET_KEY'] = "key"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
